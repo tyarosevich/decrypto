2eed7eff844d0ed89dec1966c877927cdf0ebe4f
tyarosevich <tyranosevich@gmail.com>
2022-05-14 14:26:22 -0700

    Moved on to lambda testing. Notes updated.

--- decrypto/handler_test.py
+++ decrypto/handler_test.py
@@ -5,36 +5,29 @@ import json
 from decrypto.aws_resources import get_secret
 from decrypto.db_resources import get_db_engine, db_write
 
-twitter_api_info = get_secret()
-dct_auth = json.loads(twitter_api_info['SecretString'])
-bearer_token = dct_auth['twitter_bearer']
-ret_max = 500
-query = "#bitcoin lang:en"
-lst_tweet_fields = ['lang', 'public_metrics', 'text', 'created_at']
-dct_params = {
-    'start_time': None,
-    'end_time': None,
-    'expansions': None,
-    'max_results': 10,
-    'next_token': None,
-    'tweet_fields': lst_tweet_fields
+def lambda_handler(event, context):
 
-}
-client = tweepy.Client(bearer_token, wait_on_rate_limit=True)
+    twitter_api_info = get_secret()
+    dct_auth = json.loads(twitter_api_info['SecretString'])
+    bearer_token = dct_auth['twitter_bearer']
+    ret_max = 500
+    query = "#bitcoin lang:en"
+    lst_tweet_fields = ['lang', 'public_metrics', 'text', 'created_at']
+    dct_params = {
+        'start_time': None,
+        'end_time': None,
+        'expansions': None,
+        'max_results': 10,
+        'next_token': None,
+        'tweet_fields': lst_tweet_fields
 
-def test_handler(client, query, ret_max, dct_params):
-
+    }
+    client = tweepy.Client(bearer_token, wait_on_rate_limit=True)
     df_tweet_batch = get_tweets(client, query, ret_max, dct_params)
 
-    return df_tweet_batch
+    engine = get_db_engine()
+    table = 'raw_tweets'
+    db_write(df_tweet_batch, table, engine)
 
-# TODO: Create a test table in the AWS project DB and test a function to write results to the  db.
 
-#%% test Get
 
-df = test_handler(client, query, ret_max, dct_params)
-
-#%% Test write to db
-table = 'test'
-engine = get_db_engine()
-db_write(df, table, engine)
